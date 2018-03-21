f53220fa86439c084ff2485cea1d2bbef609e6d0
Daniel Nguyen <danieltn91@gmail.com>
2018-03-20 23:22:51 -0700

    added contact form

--- send.php
+++ send.php
@@ -1,28 +1,30 @@
 <?php
-	if(!isset($_GET['submit'])){
+	if(!isset($_REQUEST['submit'])){
 		echo "error: you need to submit a form!";
 	}
 
 	$to = "danieltn91@gmail.com";    
-	$message = $_GET['message'] ;
-	$visitor_email = $_GET['email'];
-	$company = $_GET['organization'];
-	$firstName = $_GET['firstname'];
-	$lastName = $_GET['lastname'];
+	$message = $_REQUEST['message'] ;
+	$visitor_email = $_REQUEST['email'];
+	$company = $_REQUEST['organization'];
+	$firstName = $_REQUEST['firstname'];
+	$lastName = $_REQUEST['lastname'];
 	$email_subject = "New form submision";
 
-	$email_body = "You recieved a message from a user $firstName $lastName. \n",
-					"Email address: $visitor_email\n",
-					"Here is the message:\n $message",
+	$email_body = "You recieved a message from a user $firstName $lastName. \n" .
+					"Email address: $visitor_email\n" .
+					"Here is the message: "\n . $message;
 	
 	$headers = "From: $to \r\n";
 
+	$thankyou_page = "thankyou.html";
 	//validate 
 	if(empty($firstName)|| empty($lastName) || empty($visitor_email)){
 		echo "Must have name and email!";
 		exit;
 	}
 	//send email
-	mail($to, $email_subject, $email_body,$headers);
+	mail($to, $email_subject, $email_body);
+	header( "Location: $thankyou_page" );
 
 ?>
